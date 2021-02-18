0671f78632580b6e2f304b9b1b698d0998611f0d
Shane <doc.shanewilson@gmail.com>
2021-02-18 16:07:34 +0000

    Working REST API implemented

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Rest_API
 Partial code solution for week 8 lab. Contains working REST API
+
+The repository contains a working Web REST API written using ASP.NET CORE and C#. The solution offers a couple of entpoints for a simple todo items application.  
+
+You should try to implement additional endpoints for other data models. I suggest adding a _User_ model with attributes such as:
+
+* UserID
+* UserName
+* UserEmail
+* UserFirstName
+* UserSurname
+
+Your second endpoint should support CRUD requests over REST.  
---
+++ WebApplication1/WebApplication1/Startup.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Specify the database context to the DI container and register it as a service
+            services.AddDbContext<TodoContext>(opt =>
+            opt.UseInMemoryDatabase("TodoList"));
+            services.AddControllers();
+
+            //services.AddDbContext<WebApplication1Context>(options =>
+              //      options.UseSqlServer(Configuration.GetConnectionString("WebApplication1Context")));
+            /*services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApplication1", Version = "v1" });
+            });*/
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+               //app.UseSwagger();
+                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApplication1 v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            //Adds route matching to the middleware pipeline. The middleware looks at the set of endpoints
+            // defined in the app and selects the best match based on the request
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            //Add endpoint execution to the middleware pipeline. 
+            app.UseEndpoints(endpoints =>
+            {
+                // Adds endpoints for controller actions to the IEndpointRouteBuilder without specifying any routes.
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
